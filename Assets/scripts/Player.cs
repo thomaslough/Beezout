86539069d23b7b9e066bd45c9181282f9b3925a0
Thomas Lough <thomas.m.lough@gmail.com>
2021-02-16 09:26:06 -0800

    First commit

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidBody;
+
+    private float hoverX; 
+    private float hoverY;
+    private float hoverV;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            //Debug.Log("hello");
+            jumpKeyWasPressed = true;
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate() {
+        if (jumpKeyWasPressed) {
+            rigidBody.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+        rigidBody.velocity = new Vector3(horizontalInput, rigidBody.velocity.y, 0);
+    }
+}
---
+++ Assets/scripts/Props.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Props
+{
+    private int experience;
+
+    public int Experience
+    {
+        get
+        {
+            return experience;
+        }
+        set
+        {
+            experience = value;
+        }
+    }
+}
